b4c308a6322f5fd7ac57886489352b4fdb3429e0
sd44 <sd44sd44@yeah.net>
2024-04-01 20:32:55 +0800

    aa

--- auto/main.el
+++ auto/main.el
@@ -1,13 +1,6 @@
 (TeX-add-style-hook
  "main"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "data/dedication"
@@ -19,6 +12,10 @@
     "data/neoliber"
     "data/assange"
     "data/soviet"
+    "data/aboutchina"
+    "data/historychina"
+    "data/historychina2"
+    "data/historychina3"
     "dingjia"
     "dingjia10")
    (LaTeX-add-bibliographies
